560b44f9cfeb182aff13b2c079fb566427fc28b4
Damien McMahon <damien@macoto.co.uk>
2016-05-19 16:36:57 +0100

    Adds mock for event.prevendefault

--- src/app/components/add-item/add-item.directive.spec.js
+++ src/app/components/add-item/add-item.directive.spec.js
@@ -5,7 +5,7 @@
   var $scope;
 
   describe('directive checklistItem', function() {
-
+    var mockedEvent;
     beforeEach(module('angularChecklist'));
 
     beforeEach(inject(function($compile, $rootScope, $log, compileHelpers){
@@ -15,6 +15,7 @@
       el = compileHelpers.wrapElement(elMarkup);
       compileHelpers.compile(el, $scope);
       vm = el.isolateScope().vm;
+      mockedEvent = {preventDefault: function(){}};
     }));
 
     it('renders the template', function(){
@@ -23,21 +24,21 @@
 
     it('calls the passed function on `submit()`', function(){
       vm.newItem = 'A test Item';
-      vm.addItem();
+      vm.addItem(mockedEvent);
       expect($scope.add).toHaveBeenCalledWith('A test Item');
     });
 
     it('clears the value when an item has been submitted', function(){
       vm.newItem = 'Clear this Text';
-      vm.addItem();
+      vm.addItem(mockedEvent);
       expect(vm.newItem).toEqual('');
     });
 
     it('blocks the user from adding empty items', function(){
-      vm.addItem();
+      vm.addItem(mockedEvent);
       expect($scope.add).not.toHaveBeenCalled();
       vm.newItem = 'Some real text';
-      vm.addItem();
+      vm.addItem(mockedEvent);
       expect($scope.add).toHaveBeenCalledWith('Some real text');
     });
 
