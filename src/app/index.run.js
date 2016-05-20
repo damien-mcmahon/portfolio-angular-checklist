331e9ea77ca51872556a75ff60054afb520c05e1
Damien McMahon <damien@macoto.co.uk>
2016-05-20 17:32:28 +0100

    Adds constants for the new firebase API

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,7 +6,14 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log, FIREBASE_DATA_URL, FIREBASE_AUTH_DOMAIN) {
+    var config = {
+      apiKey: "AIzaSyCY4LipyQQstR0WFwBL4QhRTzCIZAj49iE",
+      authDomain: FIREBASE_AUTH_DOMAIN,
+      databaseURL: FIREBASE_DATA_URL,
+    };
+
+    firebase.initializeApp(config);
 
     $log.debug('runBlock end');
   }
