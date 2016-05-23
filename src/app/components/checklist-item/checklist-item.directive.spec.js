83a1dae95e85ad9400f6020ca11b501eaf554a32
Damien McMahon <damien@macoto.co.uk>
2016-05-23 10:04:38 +0100

    Adds a checkbox to the checklist item

--- src/app/components/checklist-item/checklist-item.directive.spec.js
+++ src/app/components/checklist-item/checklist-item.directive.spec.js
@@ -26,7 +26,11 @@
       beforeEach(inject(function($compile, _$rootScope_){
         $rootScope = _$rootScope_;
         $scope = $rootScope.$new();
-        $scope.mockItem = {title: "First Element"};
+        $scope.mockItem = {
+          title: "First Element",
+          "description": "First Element Description",
+          "isActive": true
+        };
         var elMarkup = '<checklist-item item="mockItem"></checklist-item>';
         newEl = compileHelpers.wrapElement(elMarkup);
         compileHelpers.compile(newEl, $scope);
@@ -37,6 +41,40 @@
         expect(firstEl.text().trim()).toEqual('First Element');
       });
 
+      it('renders the description', function(){
+        var firstDescription = compileHelpers.wrapElement(newEl.find('p')[0]);
+
+        expect(firstDescription.text().trim())
+          .toEqual('First Element Description');
+      });
+
+      describe('the checkbox', function(){
+        var checkboxEl;
+
+        beforeEach(function(){
+          checkboxEl = compileHelpers.wrapElement(newEl.find('input')[0]);
+        });
+
+        it('shows a checkbox in the item', function(){
+          expect(checkboxEl.html()).not.toBeUndefined();
+        });
+
+        it('allows the user to check the item off', function(){
+          expect(checkboxEl.prop('disabled')).toBe(false);
+        });
+
+        it('disables the checkbox until the item is active', function(){
+          $scope.mockItem.isActive = false;
+          $scope.$digest();
+          expect(checkboxEl.prop('disabled')).toBe(true);
+        });
+
+        it('sets the checked property for a completed item', function(){
+          $scope.mockItem.isComplete = true;
+          $scope.$digest();
+          expect(checkboxEl.prop('checked')).not.toBe(false);
+        });
+      });
     });
   });
 })();
